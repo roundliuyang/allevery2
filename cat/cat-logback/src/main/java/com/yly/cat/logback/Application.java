2b36961794b52270c005b7ac95d150e8aaf2ba96
yly <liuyang.yuan>
2024-04-27 18:24:55 +0800

    cat

---
+++ cat/cat-logback/src/main/java/com/yly/cat/logback/Application.java
@@ -0,0 +1,17 @@
+package com.yly.cat.logback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * cat-client 3.0.0 才能跑起来
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
